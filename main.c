594826e41c8a85e079231912d2d2eef3a9f09e49
Bassem Ehab Mosallam <121206244+bassembomba@users.noreply.github.com>
2023-12-22 17:58:07 +0200

    Update main.c

--- main.c
+++ main.c
@@ -3,9 +3,9 @@
 #include "hardware/adc.h"
 #include "hardware/pwm.h"
 #include "hardware/gpio.h"
-#define PWM_GPIO_PIN 17  // Replace with your actual PWM GPIO pin number
+#define PWM_GPIO_PIN 17  
 #define DIR_GPIO_PIN1 20
-#define PWM_GPIO_PIN2 16  // Replace with your actual PWM GPIO pin number
+#define PWM_GPIO_PIN2 16 
 #define DIR_GPIO_PIN3 19
 #define DIR_GPIO_PIN4 18
 
@@ -24,12 +24,10 @@ void setup_pwm(uint gpio_pin) {
 
 
 void setup() {
-
-    // Initialize ADC for moisture sensor
     adc_init();
     adc_gpio_init(sensor_pin);
     adc_select_input(0); // Select ADC input (adjust if using a different ADC channel)
-
+    
     // Initialize serial port for debugging
     stdio_init_all();
     setup_pwm(PWM_GPIO_PIN);
@@ -58,16 +56,12 @@ uint normalize(uint value, uint min_input, uint max_input, uint min_output, uint
      else if (value < min_input) {
         value = min_input;
     }
-
-
     // Map the input value to the output range
     return (value - min_input) * (max_output - min_output) / (max_input - min_input) + min_output;
 }
 
 int main() {
    setup();
-    // Replace these with your actual pin numbers
-    
 
     while (1) {
         gpio_put(DIR_GPIO_PIN1, 1);
@@ -75,11 +69,7 @@ int main() {
         gpio_put(DIR_GPIO_PIN4, 0);
 
         uint16_t analog_value = adc_read();
-          // Invert sensor value to change logic
         uint16_t inverted_sensor_value = 4095 - analog_value;
-
-        // Apply a more nuanced mapping formula for brightness
-        // Example: using a quadratic scaling to increase the variation
         uint32_t scaled_value = (inverted_sensor_value * inverted_sensor_value) / 4095;
 
         printf("Measured Value: %u        ", scaled_value);
